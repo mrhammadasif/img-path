1591243967632a6ca811a28f50bdd16a79ee0c6e
Hammad Asif <mrhammadasif@gmail.com>
2021-03-10 14:38:44 +0500

    fetch cell from K and put to T

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import axios from "axios"
+import { existsSync, mkdirSync, writeFile } from "fs"
 import { GoogleSpreadsheet } from "google-spreadsheet"
-
+import { resolve, extname } from "path"
+import { v4 as uuid } from "uuid"
 require("dotenv").config()
 
-const doc = new GoogleSpreadsheet(process.env.SHEET_KEY);
-
+const doc = new GoogleSpreadsheet(process.env.SHEET_KEY)
+const imagesDir = resolve(__dirname, "../images")
+if (!existsSync(imagesDir)) {
+  mkdirSync(imagesDir)
+}
 (async function () {
   await doc.useServiceAccountAuth({
     // eslint-disable-next-line camelcase
@@ -21,13 +27,32 @@ const doc = new GoogleSpreadsheet(process.env.SHEET_KEY);
   const sheet = doc.sheetsByIndex[0] // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
   console.log(sheet.title)
   console.log(sheet.rowCount)
-  await sheet.loadCells("K:K")
-  const cell = sheet.getCellByA1("K2")
-  console.log(cell.value)
+  await sheet.loadCells("K2:K" + sheet.rowCount)
 
-  // // adding / removing sheets
-  // const newSheet = await doc.addSheet({
-  //   title: "hot new sheet!"
-  // })
-  // await newSheet.delete()
+  for (let cellNo = 2; cellNo <= 10; cellNo ++) {
+
+    const cell = sheet.getCellByA1(`K${cellNo}`)
+    console.log(cell.value)
+    const imageUrl = cell.value.toString().split("/")
+    axios.request({
+      method: "get",
+      url: cell.value.toString(),
+      responseType: "blob"
+    }).then(((response) => {
+
+      ((response, cellNo, imageUrl) => {
+        const imagePthToWrite = uuid().toString()
+        const imageExt = extname(imageUrl.reverse()[0])
+        // `-${imagesDir}/${imageUrl[imageUrl.length - 1]}`
+        const writer = writeFile(`${imagesDir}/${imagePthToWrite}${imageExt}`, response.data, async () => {
+          await sheet.loadCells("L2:L" + sheet.rowCount)
+          const cCell = sheet.getCellByA1("L" + cellNo)
+          cCell.value = "https://tools.nitroxis.com/images/images/" + imagePthToWrite + imageExt
+          await cCell.save()
+        })
+      })(response, cellNo, imageUrl)
+      // response.data.pipe(writer)
+      // return finished(writer)
+    }))
+  }
 }())
